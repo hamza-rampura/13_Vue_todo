f67745175fa0e681704353b50da4584da4bc9f1d
Hamza Rampurawala <rampurawala_hamza@yahoo.com>
2022-05-19 21:54:32 +0530

    lint updates

--- src/App.vue
+++ src/App.vue
@@ -1,98 +1,94 @@
 <template>
-  <div id="app">
-      <HeaderMain />
-      <AddToDo v-on:add-todo = "addTodo"/>
-      <TodosBase v-bind:todos="todos" v-on:del-todo="deleteTodo" v-on:change-status="changeStatus" />
-  </div>
+	<div id="app">
+		<HeaderMain />
+		<AddToDo v-on:add-todo="addTodo" />
+		<TodosBase
+			v-bind:todos="todos"
+			v-on:del-todo="deleteTodo"
+			v-on:change-status="changeStatus"
+		/>
+	</div>
 </template>
 
 <script>
+	import TodosBase from "./components/TodosBase.vue";
+	import HeaderMain from "./components/layout/HeaderMain.vue";
+	import AddToDo from "./components/AddToDo.vue";
+	import axios from "axios";
 
-import TodosBase from "./components/TodosBase.vue";
-import HeaderMain from "./components/layout/HeaderMain.vue";
-import AddToDo from "./components/AddToDo.vue";
-import axios from "axios"
-
-export default {
-  name: 'App',
-  components: {
-      TodosBase,
-      HeaderMain,
-      AddToDo
-  },
-  data() {
-      return {
-          todos: [
-              /* {
-                  id: 1,
-                  title: "Task 1",
-                  completed: false
-              },
-              {
-                  id: 2,
-                  title: "Task 2",
-                  completed: false
-              },
-              {
-                  id: 3,
-                  title: "Task 3",
-                  completed: false
-              } */
-          ],
-          test: "Hamza"
-      }
-  },
-  methods: {
-        deleteTodo(id) {
-          axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-          .then(() => {
-                this.todos = this.todos.filter((todo)=>{
-                return todo.id !== id;
-            })}).catch(err => console.log(err))
-        },
-        changeStatus(id) {
-            for(let i = 0; i<=this.todos.length; i++) {
-                if(this.todos[i].id === id) {
-                  this.todos[i].completed = !this.todos[i].completed;
-                  return;
-                }
-            }
-        },
-        addTodo(newTodo) {
-          const { title, completed } = newTodo;
-          axios.post("https://jsonplaceholder.typicode.com/todos", {
-            title,
-            completed
-          }).then(res => this.todos = [...this.todos, res.data]).catch(err => console.log(err))
-        }
-  },
-  created() {
-    axios.get("https://jsonplaceholder.typicode.com/todos?_limit=5").then((res)=>{
-      this.todos =res.data;
-    }).catch((err)=> console.log(err))
-  }
-}
+	export default {
+		name: "App",
+		components: {
+			TodosBase,
+			HeaderMain,
+			AddToDo,
+		},
+		data() {
+			return {
+				todos: [],
+				test: "Hamza",
+			};
+		},
+		methods: {
+			deleteTodo(id) {
+				axios
+					.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+					.then(() => {
+						this.todos = this.todos.filter((todo) => {
+							return todo.id !== id;
+						});
+					})
+					.catch((err) => console.log(err));
+			},
+			changeStatus(id) {
+				for (let i = 0; i <= this.todos.length; i++) {
+					if (this.todos[i].id === id) {
+						this.todos[i].completed = !this.todos[i].completed;
+						return;
+					}
+				}
+			},
+			addTodo(newTodo) {
+				const { title, completed } = newTodo;
+				axios
+					.post("https://jsonplaceholder.typicode.com/todos", {
+						title,
+						completed,
+					})
+					.then((res) => (this.todos = [...this.todos, res.data]))
+					.catch((err) => console.log(err));
+			},
+		},
+		created() {
+			axios
+				.get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+				.then((res) => {
+          this.todos = res.data;
+				})
+				.catch((err) => console.log(err));
+		},
+	};
 </script>
 
 <style>
-* {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
-  .btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
-  .btn:hover {
-    background: #666;
-  }
+	* {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
+	body {
+		font-family: Arial, Helvetica, sans-serif;
+		line-height: 1.4;
+	}
+	.btn {
+		display: inline-block;
+		border: none;
+		background: #555;
+		color: #fff;
+		padding: 7px 20px;
+		cursor: pointer;
+	}
+	.btn:hover {
+		background: #666;
+	}
 </style>
