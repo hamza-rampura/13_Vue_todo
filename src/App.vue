7a164d0684162c5900fc5df7ed4e8e2abb944c5a
Hamza Rampurawala <rampurawala_hamza@yahoo.com>
2022-05-20 13:04:27 +0530

    routing added

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,39 @@
 <template>
 	<div id="app">
 		<HeaderMain />
-		<AddToDo v-on:add-todo="addTodo" />
-		<TodosBase
-			v-bind:todos="todos"
-			v-on:del-todo="deleteTodo"
-			v-on:change-status="changeStatus"
-		/>
+		<router-view />
 	</div>
 </template>
 
 <script>
-	import TodosBase from "./components/TodosBase.vue";
-	import HeaderMain from "./components/layout/HeaderMain.vue";
-	import AddToDo from "./components/AddToDo.vue";
-	import axios from "axios";
-
+	import HeaderMain from "./components/layout/HeaderMain";
 	export default {
-		name: "App",
+		name: "app",
 		components: {
-			TodosBase,
 			HeaderMain,
-			AddToDo,
-		},
-		data() {
-			return {
-				todos: [],
-				test: "Hamza",
-			};
-		},
-		methods: {
-			deleteTodo(id) {
-				axios
-					.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-					.then(() => {
-						this.todos = this.todos.filter((todo) => {
-							return todo.id !== id;
-						});
-					})
-					.catch((err) => console.log(err));
-			},
-			changeStatus(id) {
-				for (let i = 0; i <= this.todos.length; i++) {
-					if (this.todos[i].id === id) {
-						this.todos[i].completed = !this.todos[i].completed;
-						return;
-					}
-				}
-			},
-			addTodo(newTodo) {
-				const { title, completed } = newTodo;
-				axios
-					.post("https://jsonplaceholder.typicode.com/todos", {
-						title,
-						completed,
-					})
-					.then((res) => (this.todos = [...this.todos, res.data]))
-					.catch((err) => console.log(err));
-			},
-		},
-		created() {
-			axios
-				.get("https://jsonplaceholder.typicode.com/todos?_limit=5")
-				.then((res) => {
-          this.todos = res.data;
-				})
-				.catch((err) => console.log(err));
 		},
 	};
 </script>
 
 <style>
-	* {
-		box-sizing: border-box;
-		margin: 0;
-		padding: 0;
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
 	}
-	body {
-		font-family: Arial, Helvetica, sans-serif;
-		line-height: 1.4;
+
+	nav {
+		padding: 30px;
 	}
-	.btn {
-		display: inline-block;
-		border: none;
-		background: #555;
-		color: #fff;
-		padding: 7px 20px;
-		cursor: pointer;
+
+	nav a {
+		font-weight: bold;
+		color: #2c3e50;
 	}
-	.btn:hover {
-		background: #666;
+
+	nav a.router-link-exact-active {
+		color: #42b983;
 	}
 </style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+	<div class="about">
+		<h1>
+			This is todo list in vue 2.0 and a part of the learning journey.
+		</h1>
+	</div>
+</template>
