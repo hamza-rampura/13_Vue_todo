7da82a60466695ff6035b4eeae1446cf8aa328ac
Hamza Rampurawala <rampurawala_hamza@yahoo.com>
2022-05-20 13:06:11 +0530

    minor update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="about">
 		<h1>
-			This is todo list in vue 2.0 and a part of the learning journey.
+			This is todo list in vue 2.0 and a part of the learning journey!!
 		</h1>
 	</div>
 </template>
